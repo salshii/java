6ae69fa9e853929b23edf474cdc7c027459ef323
salshii <94897334+salshii@users.noreply.github.com>
2022-04-06 10:55:35 +0530

    tyu

---
+++ swap.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class swap
+{
+public static void main(String args[])
+{
+Scanner in= new Scanner(System.in);
+System.out.println("enter the number for A:");
+int a=in.nextInt();
+System.out.println("enter the number for B:");
+int b=in.nextInt();
+int c=a;
+a=b;
+b=c;
+System.out.println("number in A:"+a);
+System.out.println("number in B:"+b);
+
+}
+}
